53c70135e04f6826654b3b044bba54aad27ef0fe
jyp114110 <1140967775@qq.com>
2019-04-21 11:24:11 +0800

    05-node基础完成

--- 04-hackNews03/app.js
+++ 04-hackNews03/app.js
@@ -42,6 +42,51 @@ server.on('request', (req, res) => {
       }
       res.end(data)
     })
+  } else if (req.url === '/add' && req.method === 'POST') {
+    let info = ''
+    req.on('data', chunk => {
+      info += chunk
+    })
+    req.on('end', () => {
+      info = queryString.parse(info)
+      let dataObj = {}
+
+      fs.readFile(
+        path.join(__dirname, 'data', 'data.json'),
+        'utf-8',
+        (err, data) => {
+          if (err) {
+            return console.log(err)
+          }
+          dataObj = JSON.parse(data)
+          console.log(dataObj)
+          console.log(dataObj.list.length)
+
+          if (dataObj.list.length > 0) {
+            info.id = dataObj.list[dataObj.list.length - 1].id + 1
+          } else {
+            info.id = 1
+          }
+          dataObj.list.push(info)
+          console.log(dataObj)
+          let dataJson = JSON.stringify(dataObj, null, 2)
+          console.log(dataJson)
+          fs.writeFile(
+            path.join(__dirname, 'data', 'data.json'),
+            dataJson,
+            'utf-8',
+            err => {
+              if (err) {
+                return console.log(err)
+              }
+              res.statusCode = 302
+              res.setHeader('location', '/index')
+              res.end()
+            }
+          )
+        }
+      )
+    })
   } else {
     res.setHeader('content-type', 'text/html;charset=utf-8')
     res.end('404-页面未找到')
